c066a0041163277e8c861e45d0d98ae1140a8c71
prince04-coder <23bma031@nith.ac.in>
2025-01-16 23:02:57 +0530

    team page changes

---
+++ src/data/teamData.js
@@ -0,0 +1,119 @@
+import member1 from '../data/image5.jpg';
+
+const teamData = [
+    {
+      title: "Previous Club Coordinators",
+      members: [
+        {
+          name: 'Aditya',
+          image: member1,
+          links: { instagram: "#", linkedin: "#" },
+        },
+        {
+          name: "Himanshu",
+          image: member1,
+          links: { instagram: "#", linkedin: "#" },
+        },
+        // Add more coordinators
+      ],
+    },
+    {
+      title: "Pre-Final Year",
+      members: [
+        {
+          name: "Krishna Narzary",
+          image: member1,
+          links: { instagram: "#", linkedin: "#" },
+        },
+        // Add more pre-final year members
+      ],
+    },
+    {
+      title: "2nd Year",
+      members: [
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        {
+          name: "Aditya Sangal",
+          image: member1,
+          links: {
+            instagram: "https://www.linkedin.com/in/prince-khandelwal-06147a1b1",
+            linkedin: "#",
+          },
+        },
+        // Add more 2nd-year members
+      ],
+    },
+  ];
+
+  export default teamData;
